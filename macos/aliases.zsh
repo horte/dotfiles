fe5e5a81172e79e6143abdfded4006b3d47069cd
horte <fredrik@horte.se>
2017-02-03 10:10:58 +0100

    Alias for re-indexing spotlight

--- macos/aliases.zsh
+++ macos/aliases.zsh
@@ -1,2 +1,5 @@
 alias chrome="open -a 'Google Chrome'"
 alias storm="phpstorm"
+alias index="mdutil -a -i off && launchctl unload -w /System/Library/LaunchDaemons/com.apple.metadata.mds.plist \
+    && launchctl load -w /System/Library/LaunchDaemons/com.apple.metadata.mds.plist \
+    && mdutil -a -i on > /dev/null"
